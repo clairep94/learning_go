16497e1af6b8051e9ff3f0e23547525f48daeec8
Claire Peng <claire.peng@justeattakeaway.com>
2024-10-22 11:43:05 +0100

    http server in go

---
+++ 00.httpserver/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+// spin up http server
+// end point of your naming
+// accepts a json payload of { name, age }
+
+type Person struct {
+	FullName string
+	Age int
+}
+
+func doStuff(w http.ResponseWriter, req *http.Request){
+	if req.Method != "POST" {		
+		w.Write([]byte("Hello world"))
+		return
+	}
+	// reading req.body
+	buffer := new(bytes.Buffer)
+	_, err := io.Copy(buffer, req.Body)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// bytes of json --> struct
+	var p Person
+	if err := json.Unmarshal(buffer.Bytes(), &p); err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte(p.FullName + strconv.Itoa(p.Age)))
+	
+}
+
+func main() {
+	http.HandleFunc("/", doStuff)
+	http.HandleFunc("/something", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Something page"))
+	})
+
+	http.ListenAndServe(":3333", nil)
+}
