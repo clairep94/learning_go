262d511d76924558a99091808cfe0d60cb1a1172
Claire Peng <claire.peng@justeattakeaway.com>
2024-11-19 10:01:13 +0000

    add lose condition, still to do win condition

--- 000.hangmanHttpServer/main.go
+++ 000.hangmanHttpServer/main.go
@@ -10,33 +10,23 @@ import (
 	// "strconv"
 )
 const secretWord = "hidden"
+const maxGuesses = 8
 var guesses []string
-var cache = make(map[string]int)
+// var cache = make(map[string]int)
+
+type Guess struct {
+	Letter string `json:"letter"`
+}
 
 func homePage(w http.ResponseWriter, req *http.Request){
 	if req.Method != "POST" {		
 		w.Write([]byte("Hello world"))
 		return
 	}
-	// reading req.body & copy into a buffer
-	// buffer := new(bytes.Buffer)
-	// _, err := io.Copy(buffer, req.Body)
-	// if err != nil {
-	// 	w.Write([]byte(err.Error()))
-	// 	return
-	// }
-
-	// bytes of json --> struct representation of the json
-	// if err := json.Unmarshal(buffer.Bytes(), &p); err != nil {
-	// 	w.Write([]byte(err.Error()))
-	// 	return
-	// }
-	// w.Write([]byte(p.FullName + strconv.Itoa(p.Age)))
-	
 }
 
+// TODO: add lookup against cache, how to do this with Go if it'll return 0 if not in the map instead of nil?
 func indexOfElem(elem string, arr []string) int {
-	// TODO: first do a lookup against the cache
 	for i, el := range arr {
 		if elem == el {
 			return i
@@ -61,13 +51,32 @@ func DisplayConcealedWord(word string, guesses []string) string {
 	return strings.Join(display, " ")
 }
 
+func DisplayCurrentGamestate() string {
+	response := DisplayConcealedWord(secretWord, guesses) + "\n"
+	response += "Guesses: [" + strings.Join(guesses, ", ") + "]"
+	return response
+}
+
+// func CheckGame() bool {
+
+// }
+
+func isGameLost() bool {
+	return len(guesses) >= maxGuesses
+}
+
 func hangmanGame(w http.ResponseWriter, req *http.Request){
 
 	if req.Method != "POST" {
 		// Display the current game state
-		response := DisplayConcealedWord(secretWord, guesses) + "\n"
-		response += "Guesses: [" + strings.Join(guesses, ", ") + "]"
-		w.Write([]byte(response))
+		w.Write([]byte(DisplayCurrentGamestate()))
+		return
+	}
+
+	// TODO: add win condition
+	// check if already won/lost 
+	if isGameLost() {
+		http.Error(w, "Game already over", http.StatusBadRequest)
 		return
 	}
 
@@ -79,18 +88,49 @@ func hangmanGame(w http.ResponseWriter, req *http.Request){
 		return
 	}
 
-	var guess string
-	if err := json.Unmarshal(buffer.Bytes(), &guess); err != nil {
+	var g Guess
+	// valid json check
+	if err := json.Unmarshal(buffer.Bytes(), &g); err != nil {
 			w.Write([]byte(err.Error()))
 			return
 		}
-	
-	w.Write([]byte(guess))
+		
+	// single char check
+	if len(g.Letter) != 1 {
+		http.Error(w, "Invalid guess: must be a single character", http.StatusBadRequest)
+		return
+	}
+
+	g.Letter = strings.ToLower(g.Letter)
+
+	// already guessed check
+	if indexOfElem(g.Letter, guesses) != -1 {
+		http.Error(w, "Invalid guess: already guessed letter", http.StatusBadRequest)
+		return
+	} else {
+		guesses = append(guesses, g.Letter)		
+	}
+
+	// lastTurn message
+	lastTurnMessage := g.Letter
+	if indexOfElem(g.Letter, splitWord(secretWord)) == -1 {
+		lastTurnMessage += " is wrong!"
+	} else {
+		lastTurnMessage += " is in the word!"
+	}
+
+	var checkGameMessage string
+	if isGameLost() {
+		checkGameMessage = "You lost!"
+		// add win condition
+	} else {
+		checkGameMessage = "Try again!"
+	}
+
+	w.Write([]byte(DisplayCurrentGamestate() + "\n" + lastTurnMessage + "\n" + checkGameMessage))
 }
 
 func main() {
-
-	
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/hangman", hangmanGame)
 
